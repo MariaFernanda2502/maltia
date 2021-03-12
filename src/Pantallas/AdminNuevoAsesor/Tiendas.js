57afda0a198c9e9f3d0925827e302f03b5ac8c2a
MariaFernanda2502 <ferchasalvador25@gmail.com>
2021-03-11 22:30:24 -0600

    Agregando formulario AdminNuevoAsesor

---
+++ src/Pantallas/AdminNuevoAsesor/Tiendas.js
@@ -0,0 +1,12 @@
+export const Tiendas= [
+    { value: 'tienda1', label: 'Tienda 1'},
+    { value: 'tienda2', label: 'Tienda 2'},
+    { value: 'tienda3', label: 'Tienda 3'},
+    { value: 'tienda4', label: 'Tienda 4'},
+    { value: 'tienda5', label: 'Tienda 5'},
+    { value: 'tienda6', label: 'Tienda 6'},
+    { value: 'tienda7', label: 'Tienda 7'},
+    { value: 'tienda8', label: 'Tienda 8'},
+    { value: 'tienda9', label: 'Tienda 9'},
+    { value: 'tienda10', label: 'Tienda 10'},
+  ];
