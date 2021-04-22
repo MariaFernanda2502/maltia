cd8180475f5ba4f835b13a20add749e405784ba1
Victor <A01274694@itesm.mx>
2021-04-22 17:07:03 -0500

    cambios1

--- src/Pantallas/AnalistaVerPrestatarios/AnalistaVerPrestatarios.js
+++ src/Pantallas/AnalistaVerPrestatarios/AnalistaVerPrestatarios.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './AnalistaVerPrestatarios.css';
 import BotonEditar from '../../Componentes/BotonEditar/BotonEditar';
 import BotonEstatus from '../../Componentes/BotonEstatus/BotonEstatus';
 import Buscador from '../../Componentes/Buscador/Buscador';
 import FlechaRegresar from '../../Componentes/FlechaRegresar/FlechaRegresar';
 import { Link } from 'react-router-dom';
-
+import axios from 'axios';
 export default function AnalistaVerPrestatarios(props) {
+   const [status,setStatus] = useState('idle');
+    const [users,setUsers] = useState([]);
+    useEffect(() =>{
+        setStatus('loading')
+        axios.get('http://localhost:5000/analista/lista-prestatarios')
+        .then((result) =>{
+            setStatus('resolved');
+            setUsers(result.data.data)
+        })
+        .catch((error) =>{})
+    },[])
+
+    if (status === 'idle' || status === 'loading') {
+        return <h1>Cargando....</h1>
+    }
+    if (status === 'error') {
+        <h1>Hubo un error</h1>
+    }
+    if (status === 'resolved') {
     return (
         <div className="GeneralVerPrestatarios">
             <header className="BuscadorAnalistas">
@@ -20,12 +39,21 @@ export default function AnalistaVerPrestatarios(props) {
                 </div>
                 <div className="DatosPrestatarioAnalista">
                     <table>
-                        <tr className="ElementosTablaPrestatarioAnalista">
+                    {users.length > 0 && users.map((user) =>{
+                        return(
+                           <tr className="ElementosTablaPrestatarioAnalista">
                            <div className="ElementosDeTablaAnalistaprest">
-                              <td className="nombrePrestatarioAnalista">Efren Aldana Escalona</td>
+                              <td className="nombrePrestatarioAnalista" key = {user.prospectId}>{user.nombre}</td>
                               <Link to='/analista/ver-prestatarios/editar'><td className="EditarDatosAnalista"><BotonEditar /></td></Link>
-                              <td className="EstatusPesonaAnalista"><BotonEstatus/></td>
+                              <td className="EstatusPesonaAnalista"><BotonEstatus estatus="aprobado"/></td>
                            </div>
+                        </tr>  
+                        )
+                    })}
+                        <tr className="ElementosTablaPrestatarioAnalista">
+                       
+            
+        )
                         </tr>
                         <tr className="ElementosTablaPrestatarioAnalista">
                            <div className="ElementosDeTablaAnalistaprest">
@@ -60,4 +88,4 @@ export default function AnalistaVerPrestatarios(props) {
             </body>
         </div>
     );
-}
+}}
