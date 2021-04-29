f2ab6f48dd0aeb270a254d97a8f749f6b8461efe
Efren73 <A01276266@itesm.mx>
2021-04-28 21:52:23 -0500

    Adaptando codigo

--- src/Componentes/BotonGuardar/BotonGuardar.js
+++ src/Componentes/BotonGuardar/BotonGuardar.js
@@ -7,11 +7,7 @@ function handleSave() {
 function BotonGuardar() {
   return (
     <div className="BotonGuardar">
-<<<<<<< HEAD
         <button type="button" className="BotonGuardar" >Guardar</button>
-=======
-        <button type="submit" className="BotonGuardar">Guardar</button>
->>>>>>> 4a7ff5e3749b9f4f048ca9586d2a6f98632e538c
     </div>
   );
 }
